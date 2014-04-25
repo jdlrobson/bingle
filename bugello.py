8a1ac014f874b8820438b12c419e8b8d41044c3e
awjrichards <arichards@wikimedia.org>
2014-04-24 18:44:25 -0700

    Refactor Trello functionality from Bugello into Trello lib
    Adds lib/trello.py for interfacing with Trello. Note that methods in the
    lib may cause invoking scripts to exit. It would be good make that not
    happen - that is, have those methods to return some value and leave
    handling errors/bad cases up to invoking script. @TODO
    
    Resolves issue #35

--- bugello.py
+++ bugello.py
@@ -3,54 +3,10 @@
 import ConfigParser
 import sys
 import requests
-import re
 import json
 from optparse import OptionParser
 from lib.bingle import Bingle
 
-
-def getBoardId(baseParams, boardBaseName, debug=False, getLatest=False):
-    boardQueryParams = {
-        'query': boardBaseName,
-        'modelTypes': 'boards',
-        'board_fields': 'name'
-    }
-    payload = dict(baseParams.items() + boardQueryParams.items())
-    r = requests.get('https://trello.com/1/search', params=payload)
-    # don't try/except because if this fails, we can't go further anyway
-    r.raise_for_status()
-    boards = r.json()['boards']
-    if getLatest:
-        return getLatestBoardId(boards, debug)
-    else:
-        if not len(boards):
-            print "No board found by the name %s; exiting." % boardBaseName
-            sys.exit(1)
-        return boards[0]['id']
-
-
-def getLatestBoardId(boardsJson, debug=False):
-    # sprint names are like 'Mobile App - Sprint 9'; we want to find the latest
-    sprintNumRegex = re.compile('sprint (\d+)', re.I)
-    boards = []
-    for board in boardsJson:
-        sprintNum = sprintNumRegex.search(board['name'])
-        if sprintNum:
-            # do we really need name? may come in handy
-            boards.append(
-                (int(sprintNum.group(1)),
-                    board['id'],
-                    board['name'])
-            )
-    if not len(boards):
-        print "There are no valid boards for which to add cards."
-        exit(1)
-    # pick the biggest sprintNum
-    boards.sort()
-    boardId = boards[-1][1]
-    if debug:
-        print "Board name: %s" % boards[-1][2]
-    return boardId
 
 if __name__ == "__main__":
     # config stuff
@@ -59,56 +15,19 @@ if __name__ == "__main__":
                       help="Path to bugello config file", default="bugello.ini")
     (options, args) = parser.parse_args()
     config = ConfigParser.ConfigParser()
+    config.optionxform = str # needed to preserve case of option names
     config.read(options.config)
+    trelloConfig = config.items('trello')
     debug = config.getboolean('debug', 'debug')
-    appKey = config.get('trello', 'appKey')
     picklePath = config.get('paths', 'picklePath')
     product = config.get('bugzilla', 'product').split(',')
     component = config.get('bugzilla', 'component').split(',')
 
-    # check for authorization pin - this seems easier than dealing with oAuth
-    # note never expiring pin; maybe make this configurable.
-    try:
-        pin = config.get('trello', 'pin')
-    except:
-        pin = ''
-    if not len(pin):
-        pinUrl = "https://trello.com/1/authorize?key=%s&name=%s&expiration=never&response_type=token&scope=read,write" % \
-            (appKey, config.get('trello', 'appName'))
-        print "You must add a valid Trello user PIN to your config file."
-        print "To get a Trello user PIN, visit:"
-        print "%s" % pinUrl
-        sys.exit(1)
-
-    # params needed by all trello api reqs
-    baseParams = {
-        'key': appKey,
-        'token': pin,
-    }
-
-    boardId = getBoardId(baseParams, config.get(
-        'trello', 'boardBaseName'), debug, config.getboolean('trello', 'useLatestBoard'))
-    # determine current board
-
-    # determine 'Ready for Dev' list
-    # @TODO is this *always* the name of the list to use?
-    tListUrl = 'https://trello.com/1/boards/%s/lists' % boardId
-    r = requests.get(tListUrl, params=baseParams)
-    # don't try/except, because we can't do anything without this.
-    r.raise_for_status()
-    tListId = None
-    tListName = config.get('trello', 'targetListName')
-    for tList in r.json():
-        if tList['name'].lower() == tListName.lower():
-            tListId = tList['id']
-            if debug:
-                print "List id: %s" % tListId
-            break
-    if not tListId:
-        print "Could not find list: %s" % tListName
-        sys.exit(1)
+    trello = Trello(config=trelloConfig, debug=debug)
+    tListId = trello.getBugListId()
 
     bingle = Bingle(debug=debug, picklePath=picklePath)
+    # @TODO update to use API rather than feeds
     fromTime = bingle.getTimeFromPickle()
     params = {
         'product': product,
@@ -126,25 +45,12 @@ if __name__ == "__main__":
     for entry in bingle.getBugEntries(bugzillaPayload):
         # 1 look for existence of the card
         cardTitle = entry.get('summary').encode('UTF-8', 'ignore')
-        cardQueryParams = {
-            'query': cardTitle,
-            'card_fields': 'name',
-            'modelTypes': 'cards'
-        }
-        payload = dict(baseParams.items() + cardQueryParams.items())
-        try:
-            r = requests.get('https://trello.com/1/search', params=payload)
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            if debug:
-                print "Error querying for: %s" % cardTitle
-                print "Reason: %s" % e
-            continue
+        cards = trello.searchCardsByName(cardTitle)
         # check if we actually have a match
         # it looks like the API search query might do a fuzzy search, so we want to
         # make sure we only get a full match
         cardExists = False
-        for card in r.json()['cards']:
+        for card in cards:
             if card['name'] == cardTitle:
                 cardExists = True
         if cardExists:
@@ -154,20 +60,5 @@ if __name__ == "__main__":
 
         bugUrl = 'https://bugzilla.wikimedia.org/%s' % entry.get('id')
         # add card to current board
-        newCardParams = {
-            'name': cardTitle,
-            'desc': bugUrl,
-            'idList': tListId,
-            'due': None
-        }
-        payload = dict(baseParams.items() + newCardParams.items())
-        try:
-            r = requests.post('https://trello.com/1/cards', params=payload)
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            if debug:
-                print "Error adding card: %s" % cardTitle
-                print "Reason: %s" % e
-            sys.exit(-1)
-            continue
+        trello.postNewCard(cardTitle, bugUrl, tListId)
     bingle.updatePickleTime()
---
+++ lib/trello.py
@@ -0,0 +1,178 @@
+import requests
+import re
+import sys
+
+class Trello:
+    appKey = None
+    appName = None
+    boardBaseName = None
+    targetListName = None
+    pin = None
+    useLatestBoard = False
+    debug = False
+
+    def __init__(self, config=None, debug=False):
+        if config:
+            self.loadConfig(config)
+        if debug is True:
+            self.debug = True
+        self.pinCheck()
+
+    def getBaseParams(self):
+        return {
+            'key': self.appKey,
+            'token': self.pin
+        }
+
+    def loadConfig(self, config):
+        # expects a list of tuples from ConfigParser.items()
+        truths = (1, 'yes', 'true', 'True', 'on', True, '1')
+        validConfigParams = (
+            'appKey',
+            'appName',
+            'boardBaseName',
+            'targetListName',
+            'pin',
+            'useLatestBoard'
+        )
+        for items in config:
+            if items[0] not in validConfigParams:
+                continue
+            elif items[0] == 'useLatestBoard':
+                if items[1] in truths:
+                    self.useLatestBoard = True
+            else:
+                setattr(self, items[0], items[1])
+
+    def getBoardId(self):
+        boardQueryParams = {
+            'query': self.boardBaseName,
+            'modelTypes': 'boards',
+            'board_fields': 'name'
+        }
+        payload = dict(self.getBaseParams().items() + boardQueryParams.items())
+        r = requests.get('https://trello.com/1/search', params=payload)
+        # don't try/except because if this fails, we can't go further anyway
+        r.raise_for_status()
+        boards = r.json()['boards']
+        if self.useLatestBoard:
+            if self.debug:
+                print "Searching for latest board..."
+            return self.getLatestBoardId(boards)
+        else:
+            if not len(boards):
+                print "No board found by the name %s; exiting." % self.boardBaseName
+                sys.exit(1)
+            boardId = None
+            # in case the query returned multiple boards, pick the right one
+            for board in boards:
+                if board['name'] == self.boardBaseName:
+                    boardId = board['id']
+            if not boardId:
+                print "Could not find board by name %s." % self.boardBaseName
+                sys.exit(1)
+        if self.debug:
+            print "Using board ID %s" % boards[0]['id']
+        return boardId
+
+    def getLatestBoardId(self, boardsJson):
+        # sprint names are like: 'Mobile App - Sprint 9'
+        # we want to find the latest
+        sprintNumRegex = re.compile('sprint (\d+)', re.I)
+        boards = []
+        for board in boardsJson:
+            sprintNum = sprintNumRegex.search(board['name'])
+            if sprintNum:
+                # do we really need name? may come in handy
+                boards.append(
+                    (int(sprintNum.group(1)),
+                        board['id'],
+                        board['name'])
+                )
+        if not len(boards):
+            print "There are no valid boards for which to add cards."
+            exit(1)
+        # pick the biggest sprintNum
+        boards.sort()
+        boardId = boards[-1][1]
+        if self.debug:
+            print "Board name: %s" % boards[-1][2]
+        return boardId
+
+    def searchCardsByName(self, cardTitle):
+        cards = None
+        cardQueryParams = {
+            'query': cardTitle,
+            'card_fields': 'name',
+            'modelTypes': 'cards'
+        }
+        payload = dict(self.getBaseParams().items() + cardQueryParams.items())
+        try:
+            r = requests.get('https://trello.com/1/search', params=payload)
+            r.raise_for_status()
+            cards = r.json()['cards']
+        except requests.exceptions.HTTPError as e:
+            if self.debug:
+                print "Error querying for: %s" % cardTitle
+                print "Reason: %s" % e
+        return cards
+
+    def postNewCard(self, cardTitle, bugUrl, tListId):
+        newCardParams = {
+            'name': cardTitle,
+            'desc': bugUrl,
+            'idList': tListId,
+            'due': None
+        }
+        payload = dict(self.getBaseParams().items() + newCardParams.items())
+        try:
+            r = requests.post('https://trello.com/1/cards', params=payload)
+            r.raise_for_status()
+            return r
+        except requests.exceptions.HTTPError as e:
+            if self.debug:
+                print "Error adding card: %s" % cardTitle
+                print "Reason: %s" % e
+            sys.exit(-1)
+
+    def pinCheck(self):
+        # check for authorization pin
+        # note that this seems easier than dealing with oAuth
+        # note never expiring pin -
+        # maybe make this configurable.
+        if not self.pin:
+            self.pinPrompt()
+
+    def pinPrompt(self):
+        pinUrl = "https://trello.com/1/authorize?key=%s&name=%s&expiration=never&response_type=token&scope=read,write" % \
+            (self.appKey, self.appName)
+        print "You must add a valid Trello user PIN to your config file."
+        print "To get a Trello user PIN, visit:"
+        print "%s" % pinUrl
+        sys.exit(1)
+
+    def getListIdByName(self, name):
+        tListId = None
+        boardId = self.getBoardId()
+        tListUrl = 'https://trello.com/1/boards/%s/lists' % boardId
+        r = requests.get(tListUrl, params=self.getBaseParams())
+        # don't try/except, because we can't do anything without this.
+        r.raise_for_status()
+        tListId = None
+        for tList in r.json():
+            if tList['name'].lower() == name.lower():
+                tListId = tList['id']
+                if self.debug:
+                    print "List id: %s" % tListId
+                break
+        if not tListId:
+            print "Could not find list: %s" % name
+            sys.exit(1)
+        return tListId
+
+    def getBugListId(self):
+        if not self.targetListName:
+            print "No target list name set in config file."
+            sys.exit(1)
+        return self.getListIdByName(self.targetListName)
+
